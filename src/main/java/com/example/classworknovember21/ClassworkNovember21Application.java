d0359470d0d323b77e75d6260f8e0c5b7105f57d
BBFerdinandov18 <BBFerdinandov18@codingburgas.bg>
2022-11-21 11:34:05 +0200

    Added Spring MVC application part 1

---
+++ src/main/java/com/example/classworknovember21/ClassworkNovember21Application.java
@@ -0,0 +1,13 @@
+package com.example.classworknovember21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassworkNovember21Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassworkNovember21Application.class, args);
+    }
+
+}
